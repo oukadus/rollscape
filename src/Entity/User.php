22f1a6ca77961c8729b1ce6736992fb15612981a
Abdelkader Ounadjela <abdelkader.ounadjela@gmail.com>
2025-07-05 18:58:03 +0200

    add unique email validation to User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,16 +2,21 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(
+    fields: ['email'],
+    message: 'Un compte avec cette adresse email existe déjà.'
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -24,81 +24,81 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('firstname', TextType::class, [
-            'label' => 'Prénom',
-            'attr' => ['placeholder' => 'Indiquez votre prénom'],
-            'constraints' => [
-                new NotBlank(['message' => 'Le prénom est obligatoire']),
-                new Length([
-                    'min' => 3,
-                    'minMessage' => 'Le prénom doit contenir au moins {{limit}} caractères',
-                    'max' => 30,
-                    'maxMessage' => 'Le prénom doit contenir au plus {{limit}} caractères'
-                ])
-            ]
-        ])
-        ->add('lastname', TextType::class, [
-            'label' => 'Nom',
-            'attr' => ['placeholder' => 'Indiquez votre nom'],
-            'constraints' => [
-                new NotBlank(['message' => 'Le nom est obligatoire']),
-                new Length([
-                    'min' => 3,
-                    'minMessage' => 'Le nom doit contenir au moins {{limit}} caractères',
-                    'max' => 30,
-                    'maxMessage' => 'Le nom doit contenir au plus {{limit}} caractères'
-                ])
-            ]
-        ])
-        ->add('email', EmailType::class, [
-            'label' => 'Email',
-            'attr' => ['placeholder' => 'Indiquez votre adresse email'],
-            'required' => true,
-            'constraints' => [
-                new NotBlank(['message' => 'L\'email est obligatoire']),
-                new Email(['message' => 'Veuillez entrer une adresse email valide.']),
-            ]
-        ])
-        ->add('password', RepeatedType::class, [
-            'type' => PasswordType::class,
-            'required' => true,
-            'first_options' => [
-                'label' => 'Choississez votre mot de passe',
-                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Indiquez votre prénom'],
                 'constraints' => [
-                    new NotBlank(['message' => 'Le mot de passe est obligatoire']),
+                    new NotBlank(['message' => 'Le prénom est obligatoire']),
                     new Length([
-                        'min' => 8,
-                        'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères',
-                        'max' => 14,
-                        'maxMessage' => 'Le mot de passe doit contenir au plus 14 caractères',
-                    ]),
-                    new Regex([
+                        'min' => 3,
+                        'minMessage' => 'Le prénom doit contenir au moins {{limit}} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Le prénom doit contenir au plus {{limit}} caractères'
+                    ])
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Indiquez votre nom'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le nom est obligatoire']),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le nom doit contenir au moins {{limit}} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Le nom doit contenir au plus {{limit}} caractères'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Indiquez votre adresse email'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'L\'email est obligatoire']),
+                    new Email(['message' => 'Veuillez entrer une adresse email valide.']),
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Choississez votre mot de passe',
+                    'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                    'constraints' => [
+                        new NotBlank(['message' => 'Le mot de passe est obligatoire']),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères',
+                            'max' => 14,
+                            'maxMessage' => 'Le mot de passe doit contenir au plus 14 caractères',
+                        ]),
+                        new Regex([
                             'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
                             'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule et un chiffre.',
                         ])
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre mot de passe',
+
                 ]
-            ],
-            'second_options' => [
-                'label' => 'Confirmer votre mot de passe',
-                
-            ]
-        ])
-        ->add('agreeTerms', CheckboxType::class, [
-            'label' => false,
-            'mapped' => false, // ce champ ne correspond pas à une propriété de l'entité User
-            'constraints' => [
-                new IsTrue([
-                    'message' => 'Vous devez accepter les conditions pour créer un compte.',
-                ]),
-            ],
-        ])
-        ->add('submit', SubmitType::class, [
-            'label' => 'S\'inscrire', 
-            'attr' => [
-                'class' => 'btn btn-rs-primary w-100 mt-2',
-            ]
-        ])
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => false,
+                'mapped' => false, // ce champ ne correspond pas à une propriété de l'entité User
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions pour créer un compte.',
+                    ]),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => [
+                    'class' => 'btn btn-rs-primary w-100 mt-2',
+                ]
+            ])
         ;
     }
 
